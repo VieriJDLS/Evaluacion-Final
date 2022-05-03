9288a833297b4fbebd6b900a63a695ded6e75b4b
VieriJDLS <100221045+VieriJDLS@users.noreply.github.com>
2022-05-03 09:18:03 -0600

    update main.py
    Actualización del codigo de python

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ print('Nombre: Vieri Jacob de León Sosa')
 print('Carné: 1517122')
 print('Ingeniería en Informatica y Sistemas')
 print('Universidad Rafael Landivar')
+
+animal = input('Que animal es tu favorito?: ')
+
+if animal == 'delfin':
+    print('Mi animal favorito tambien es el delfin')
+elif animal != 'delfin':
+    print('Ese es un animal muy interesante, pero me gusta más el delfin')
